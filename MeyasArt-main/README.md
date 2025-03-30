710f0a1e558d1292ba06c04e63a5546e1cbc2949
AMIT DOLIYA <130552073+amitdoliya@users.noreply.github.com>
2025-03-30 18:13:12 +0530

    second

---
+++ MeyasArt-main/README.md
@@ -0,0 +1,1 @@
+# MeyasArt
---
+++ MeyasArt-main/script.js
@@ -0,0 +1,5 @@
+document.getElementById("clickText").addEventListener("click", function() {
+      var overlay = document.getElementById("overlay");
+      overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  });
+  
